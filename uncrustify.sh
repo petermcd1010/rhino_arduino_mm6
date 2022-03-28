25f608966be85e6d83b831915eaf9c25012d345e
Peter McD <redacted@null.invalid>
2022-03-27 21:22:57 -0600

    Add uncrustify.{cfg,sh}.

---
+++ uncrustify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Requires uncrustify which, in turn, is built with cmake. See the following links:
+#   https://github.com/uncrustify/uncrustify
+#   https://cmake.org/install/
+#
+
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if [ -h "${SCRIPT_PATH}" ]; then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+
+cd $SCRIPT_PATH
+
+uncrustify --no-backup -c uncrustify.cfg *.h *.cpp *.ino
+
+exit 0
