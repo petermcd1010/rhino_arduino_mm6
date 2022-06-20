915ee6b7608af960158bdf9f8455255c5cf8d550
Peter McD <redacted@null.invalid>
2022-06-19 22:02:44 -0600

    Remove unused calibrate_data_t.

--- calibrate.h
+++ calibrate.h
@@ -6,14 +6,5 @@
 
 #include "motor.h"
 
-typedef struct {
-    int min_encoder;
-    int max_encoder;
-    int home_forward_on_encoder;  // Home switch forward direction high value.
-    int home_forward_off_encoder;  // Home switch forward direction low value.
-    int home_reverse_on_encoder;  // Home switch reverse direction high value.
-    int home_reverse_off_encoder;  // Home switch reverse direction low value.
-} calibrate_data_t;
-
 void calibrate_home_switch_and_limits(int motor_ids_mask, int max_speed_percent);
 void calibrate_home_switch(int motor_ids_mask, int max_speed_percent);
